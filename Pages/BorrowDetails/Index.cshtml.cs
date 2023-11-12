95ca3222bf919e53e689ac0300f9acd0c1d0a880
Vinaz <125428096+vinh23052002@users.noreply.github.com>
2023-11-12 18:00:29 +0700

    Done Filter BorrowDetail

--- Pages/BorrowDetails/Index.cshtml.cs
+++ Pages/BorrowDetails/Index.cshtml.cs
@@ -12,6 +12,8 @@ using Microsoft.AspNetCore.SignalR;
 using Library_System.Hubs;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Security.Claims;
+using System.Globalization;
+using System.Text;
 
 namespace Library_System.Pages.BorrowDetails
 {
@@ -27,46 +29,118 @@ namespace Library_System.Pages.BorrowDetails
         }
         [BindProperty]
         public IList<BorrowDetail> BorrowDetail { get; set; } = default!;
+        [BindProperty]
+        public string status { get; set; } = default!;
+		[BindProperty]
+        public string search { get; set; } = default!;
+		[BindProperty]
+        public int option { get; set; } = default!;
+		[BindProperty]
+		public int optionDate { get; set; } = default!;
+		[BindProperty]
+        public DateTime startDate { get; set; } = default!;
+		[BindProperty]
+        public DateTime endDate { get; set; } = default!;
 
-        public async Task OnGetAsync()
+		public async Task OnGetAsync()
         {
-            var account = HttpContext.User;
-            // get username off account
-            var userName = account.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            if (_context.BorrowDetails != null)
-            {
-                if(User.Claims.SingleOrDefault(c => c.Type == "isAdmin")?.Value == "True")
-                {
-                    BorrowDetail = await _context.BorrowDetails
-                    .Include(b => b.Account)
-                    .Include(b => b.Book)
-                    .ToListAsync();
-                }
-                else
-                {
-                    BorrowDetail = await _context.BorrowDetails
-                    .Include(b => b.Account)
-                    .Include(b => b.Book)
-                    .Where(o => o.Account.UserName == userName)
-                    .ToListAsync();
-                }
+			await getDataStart();
+			setStartOptionFilter();
+
+		}
+        public async Task getDataStart()
+        {
+			var account = HttpContext.User;
+			// get username off account
+			var userName = account.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+			if (_context.BorrowDetails != null)
+			{
+				if (User.Claims.SingleOrDefault(c => c.Type == "isAdmin")?.Value == "True")
+				{
+					BorrowDetail = await _context.BorrowDetails
+					.Include(b => b.Account)
+					.Include(b => b.Book)
+					.ToListAsync();
+				}
+				else
+				{
+					BorrowDetail = await _context.BorrowDetails
+					.Include(b => b.Account)
+					.Include(b => b.Book)
+					.Where(o => o.Account.UserName == userName)
+					.ToListAsync();
+				}
 
 
-                foreach (var item in BorrowDetail.Where(item => item.ReturnDate <= DateTime.Now && item.Status == "Borrowed"))
-                {
-                    item.Status = "OutOfDate";
-                    _context.BorrowDetails.Update(item);
-                    await _context.SaveChangesAsync();
-                    
-                }
+				foreach (var item in BorrowDetail.Where(item => item.ReturnDate <= DateTime.Now && item.Status == "Borrowed"))
+				{
+					item.Status = "OutOfDate";
+					_context.BorrowDetails.Update(item);
+					await _context.SaveChangesAsync();
 
-            }
-            List<string> status = new List<string>() {"Booked", "Pending", "Borrowed", "OutOfDate", "Returned", "Canceled", };
-            ViewData["Status"] = new SelectList(status);
+				}
+
+			}
+			List<string> status = new List<string>() { "Booked", "Pending", "Borrowed", "OutOfDate", "Returned", "Canceled", };
+			ViewData["Status"] = new SelectList(status);
+		}
+        public void setStartOptionFilter()
+        {
+            status = "All";
+            option = 1;
+            search = "";
+            optionDate = 1;
+            startDate = default!;
+            endDate = DateTime.Now;
 
         }
+        public async Task OnPostSearch()
+        {
+			await getDataStart();
+			search = RemoveDiacritics(search).ToLower().Trim();
+            
+            if(status != "All")
+            {
+				BorrowDetail = BorrowDetail.Where(o => o.Status == status).ToList();
+			}
+            if (option == 1)
+            {
+                BorrowDetail = BorrowDetail.Where(o => o.Account.UserName.ToLower().Contains(search)).ToList();
+            }
+            else if(option ==2)
+            {
+                BorrowDetail = BorrowDetail.Where(o => RemoveDiacritics(o.Book.BookName.ToLower().Trim()).Contains(search)).ToList();
+            }
+			if(optionDate == 1)
+			{
+				BorrowDetail = BorrowDetail.Where(o => o.BorrowDate.Date >= startDate.Date && o.BorrowDate.Date <= endDate.Date).ToList();
+			}
+			else if(optionDate == 2)
+			{
+				BorrowDetail = BorrowDetail.Where(o => o.ReturnDate.Date >= startDate.Date && o.ReturnDate.Date <= endDate.Date).ToList();
+			}
+            
+			
+		}
+		public static string RemoveDiacritics(string text)
+		{
+            text = text == null ? "" : text;
+			var normalizedString = text.Normalize(NormalizationForm.FormD);
+			var stringBuilder = new StringBuilder();
 
-        public IActionResult OnPostExtention(int? id)
+			foreach (var c in normalizedString)
+			{
+				var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+				if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+				{
+					stringBuilder.Append(c);
+				}
+			}
+
+			return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+		}
+
+		public IActionResult OnPostExtention(int? id)
         {
             var borrowDetail = _context.BorrowDetails.Where(c=> c.BorrowId == id).SingleOrDefault();
             // Add 7 days to return date
